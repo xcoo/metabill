ece0ac4eb588299be02882d60b32fb1a207e1325
kbaba1001 <kbaba1001@users.noreply.github.com>
2021-10-26 16:47:25 +0900

    feat: lein-metabill

--- lein-metabill/project.clj
+++ lein-metabill/project.clj
@@ -1,6 +1,6 @@
-(defproject lein-metabill "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+(defproject xcoo/lein-metabill "0.1.0"
+  :description "Read build meta info"
+  :url "https://github.com/xcoo/metabill"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :eval-in-leiningen true)
