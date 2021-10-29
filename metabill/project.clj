839079dc9b5fcae901c5685993848ee53aea9d6c
kbaba1001 <kbaba1001@users.noreply.github.com>
2021-10-29 16:48:25 +0900

    chore: change description

--- metabill/project.clj
+++ metabill/project.clj
@@ -1,5 +1,5 @@
 (defproject xcoo/metabill "0.1.0"
-  :description "Read build timestamp from JAR/WAR"
+  :description "Read build meta info"
   :url "https://github.com/xcoo/metabill"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
