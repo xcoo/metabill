a23db0e1ee5c39c83503df47f3af9fe227c6c642
kbaba1001 <kbaba1001@users.noreply.github.com>
2021-11-24 13:58:37 +0900

    fix: package dependencies

--- lein-metabill/project.clj
+++ lein-metabill/project.clj
@@ -1,7 +1,7 @@
-(defproject jp.xcoo/lein-metabill "0.1.0"
+(defproject jp.xcoo/lein-metabill "0.1.1-SNAPSHOT"
   :description "Read build meta info"
   :url "https://github.com/xcoo/metabill"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[xcoo/metabill "0.1.0"]]
+  :dependencies [[jp.xcoo/metabill "0.1.1-SNAPSHOT"]]
   :eval-in-leiningen true)
--- metabill/project.clj
+++ metabill/project.clj
@@ -1,4 +1,4 @@
-(defproject jp.xcoo/metabill "0.1.0"
+(defproject jp.xcoo/metabill "0.1.1-SNAPSHOT"
   :description "Read build meta info"
   :url "https://github.com/xcoo/metabill"
   :license {:name "Apache License, Version 2.0"
