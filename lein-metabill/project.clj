ad0631083bd8969966873af028cf09c5dbe36068
kbaba1001 <kbaba1001@users.noreply.github.com>
2021-11-29 11:10:04 +0900

    Revert "chore: remove gpg"
    This reverts commit eaa90336a99229e8609abab968b3501e9a63d6be.

--- lein-metabill/project.clj
+++ lein-metabill/project.clj
@@ -3,5 +3,6 @@
   :url "https://github.com/xcoo/metabill"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[jp.xcoo/metabill "0.1.1"]]
-  :eval-in-leiningen true)
+  :dependencies [[xcoo/metabill "0.1.0"]]
+  :eval-in-leiningen true
+  :signing {:gpg-key "developer@xcoo.jp"})
--- metabill/project.clj
+++ metabill/project.clj
@@ -5,4 +5,5 @@
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies []
   :repl-options {:init-ns metabill.core}
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]]}})
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]]}}
+  :signing {:gpg-key "developer@xcoo.jp"})
