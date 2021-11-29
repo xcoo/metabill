15edbac46ed14378cd08cd06856ecfa78ecf5d09
kbaba1001 <kbaba1001@users.noreply.github.com>
2021-11-29 17:34:50 +0900

    builds: update dependencies

--- lein-metabill/project.clj
+++ lein-metabill/project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/xcoo/metabill"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[xcoo/metabill "0.1.0"]]
+  :dependencies [[jp.xcoo/metabill "0.1.2-SNAPSHOT"]]
   :eval-in-leiningen true
   :signing {:gpg-key "developer@xcoo.jp"})
