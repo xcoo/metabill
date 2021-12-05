5f0d4da94e0bc39bdd1a0d3e0d3b90d79d7a910b
Takashi AOKI <me@tak.sh>
2021-12-05 13:30:35 +0900

    chore: release 0.1.2

--- lein-metabill/project.clj
+++ lein-metabill/project.clj
@@ -1,8 +1,8 @@
-(defproject jp.xcoo/lein-metabill "0.1.2-SNAPSHOT"
+(defproject jp.xcoo/lein-metabill "0.1.2"
   :description "Read build meta info"
   :url "https://github.com/xcoo/metabill"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[jp.xcoo/metabill "0.1.2-SNAPSHOT"]]
+  :dependencies [[jp.xcoo/metabill "0.1.2"]]
   :eval-in-leiningen true
   :signing {:gpg-key "developer@xcoo.jp"})
